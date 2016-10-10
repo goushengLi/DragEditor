1d13c9e4311c9e5f79782c9c16625a0f7fc04415
李狗剩 <sheyongcan@gmail.com>
2016-10-10 23:44:09 +0800

    2016.10.10 23.44

---
+++ app/src/main/java/com/goushengli/drageditor/base/BaseFragment.java
@@ -0,0 +1,31 @@
+package com.goushengli.drageditor.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by goushengLi on 2016/10/10.
+ */
+
+public abstract class BaseFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return createView(inflater, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+    }
+
+    public abstract View createView(LayoutInflater inflater, @Nullable Bundle savedInstanceState);
+
+    public abstract void initData();
+}
---
+++ app/src/main/java/com/goushengli/drageditor/fragment/EditorMainFragment.java
@@ -0,0 +1,25 @@
+package com.goushengli.drageditor.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.goushengli.drageditor.base.BaseFragment;
+
+/**
+ * Created by goushengLi on 2016/10/10.
+ */
+
+public class EditorMainFragment extends BaseFragment {
+
+    @Override
+    public View createView(LayoutInflater inflater, @Nullable Bundle savedInstanceState) {
+        return null;
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
